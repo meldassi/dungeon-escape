f303eed1e45c0188855ca1c659cb965f5b6267e6
Aluno SENAI <Aluno@SN-405073.edu.fiesc.com.br>
2026-06-02 17:10:30 -0300

    inimigo

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 
 int main () {
 	
-	Personagem heroi ( " Heroi" , 100 , 10 ) ;
-	heroi.apresentar () ;
+	Personagem heroi ( " Heroi , 100 , 10 ") ;
+	heroi.apresenta () ;
+	
+	
+	inimigo goblin("goblin" , 10 , 5 , "goblin");
+	goblin.aprensentar(); 
+	
+	
 	return 0;
+	
 }
